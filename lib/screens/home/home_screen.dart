4fd764c7db3e015296c1497ac733a4c70b28622c
DongSeong Seo <dong.seong.jake.seo@gmail.com>
2021-04-01 18:01:25 +0900

    done with the tutorial for the website itself, started with responsive design

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:news/constants.dart';
 import 'package:news/models/Blog.dart';
+import 'package:news/screens/home/components/search.dart';
+
+import 'components/blog_post.dart';
+import 'components/categories.dart';
+import 'components/recent_posts.dart';
+import 'components/side_bar_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({
@@ -10,66 +17,25 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Expanded(
           flex: 2,
           child: Column(
-            children: [
-              AspectRatio(
-                aspectRatio: 1.78,
-                child: Image.asset(
-                  blogPosts[0].image,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(kDefaultPadding),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(10),
-                    bottomRight: Radius.circular(10),
-                  ),
-                ),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          "Design".toUpperCase(),
-                          style: TextStyle(
-                            color: kDarkBlackColor,
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(width: kDefaultPadding),
-                        Text(
-                          blogPosts[0].date,
-                          style: Theme.of(context).textTheme.caption,
-                        ),
-                      ],
-                    ),
-                    Text(
-                      blogPosts[0].title,
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontFamily: "Raleway",
-                        color: kDarkBlackColor,
-                        height: 1.3,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+            children: List.generate(blogPosts.length,
+                (index) => BlogPostCard(blog: blogPosts[index])),
           ),
         ),
         SizedBox(width: kDefaultPadding),
         Expanded(
-          child: Container(
-            height: 400,
-            color: Colors.blueGrey,
+          child: Column(
+            children: [
+              Search(),
+              SizedBox(height: kDefaultPadding),
+              Categories(),
+              SizedBox(height: kDefaultPadding),
+              RecentPosts()
+            ],
           ),
         ),
       ],
--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,13 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
 import 'package:news/constants.dart';
+import 'package:news/controllers/MenuController.dart';
 import 'package:news/screens/home/home_screen.dart';
 
 import 'components/header.dart';
 
 class MainScreen extends StatelessWidget {
+  final MenuController _controller = Get.put(MenuController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _controller.scaffoldkey,
+      drawer: Drawer(
+        child: Container(
+          color: kDarkBlackColor,
+          child: Obx(
+            () => ListView(
+              children: [
+                DrawerHeader(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: kDefaultPadding * 3.5,
+                    ),
+                    child: SvgPicture.asset("assets/icons/logo.svg"),
+                  ),
+                ),
+                ...List.generate(
+                  _controller.menuItems.length,
+                  (index) => DrawerItem(
+                    isActive: index == _controller.selectedIndex,
+                    title: _controller.menuItems[index],
+                    press: () {
+                      _controller.setMenuIndex(index);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -19,6 +53,32 @@ class MainScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class DrawerItem extends StatelessWidget {
+  final String title;
+  final bool isActive;
+  final VoidCallback press;
+  const DrawerItem({
+    Key key,
+    @required this.title,
+    @required this.isActive,
+    @required this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      selected: isActive,
+      selectedTileColor: kPrimaryColor,
+      onTap: press,
+      title: Text(
+        title,
+        style: TextStyle(color: Colors.white),
       ),
     );
   }
